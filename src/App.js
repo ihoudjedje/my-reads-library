292aaa582d9cf0ddf6cf02ed2ebbb2255f01738b
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-03-30 13:51:32 +0100

    chore: Install react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
+import { Route } from "react-router-dom";
 import BookComp from "./components/BookComp";
 import BookshelfTitleComp from "./components/BookshelfTitleComp";
 import ListBooksTitleComponent from "./components/ListBooksTitleComponent.js";
@@ -13,7 +14,6 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false,
     currentlyReadingBookshelfData: [],
     wandToReadBookshelfData: [],
     readBookshelfData: []
@@ -70,97 +70,82 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <ListBooksTitleComponent title="MyReads" />
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <BookshelfTitleComp title="Currently Reading" />
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {currentlyReadingBookshelfData.map(book => (
-                        <li key={book.id}>
-                          <BookComp
-                            bookId={book.id}
-                            bookTitle={book.title}
-                            bookAuthors={book.authors[0]}
-                            bookCoverUrl={book.imageLinks.thumbnail}
-                            currentBookshelf={book.shelf}
-                            onChangeBookshelf={this.onChangeBookshelf}
-                            bookCoverWidth={128}
-                            bookCoverHeight={193}
-                          />
-                        </li>
-                      ))}
-                    </ol>
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBooks onChangeBookshelf={this.onChangeBookshelf} />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div className="list-books">
+              <ListBooksTitleComponent title="MyReads" />
+              <div className="list-books-content">
+                <div>
+                  <div className="bookshelf">
+                    <BookshelfTitleComp title="Currently Reading" />
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        {currentlyReadingBookshelfData.map(book => (
+                          <li key={book.id}>
+                            <BookComp
+                              bookId={book.id}
+                              bookTitle={book.title}
+                              bookAuthors={book.authors[0]}
+                              bookCoverUrl={book.imageLinks.thumbnail}
+                              currentBookshelf={book.shelf}
+                              onChangeBookshelf={this.onChangeBookshelf}
+                              bookCoverWidth={128}
+                              bookCoverHeight={193}
+                            />
+                          </li>
+                        ))}
+                      </ol>
+                    </div>
                   </div>
-                </div>
-                <div className="bookshelf">
-                  <BookshelfTitleComp title="Want to Read" />
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {wandToReadBookshelfData.map(book => (
-                        <li key={book.id}>
-                          <BookComp
-                            bookId={book.id}
-                            bookTitle={book.title}
-                            bookAuthors={book.authors[0]}
-                            bookCoverUrl={book.imageLinks.thumbnail}
-                            currentBookshelf={book.shelf}
-                            onChangeBookshelf={this.onChangeBookshelf}
-                            bookCoverWidth={128}
-                            bookCoverHeight={193}
-                          />
-                        </li>
-                      ))}
-                    </ol>
+                  <div className="bookshelf">
+                    <BookshelfTitleComp title="Want to Read" />
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        {wandToReadBookshelfData.map(book => (
+                          <li key={book.id}>
+                            <BookComp
+                              bookId={book.id}
+                              bookTitle={book.title}
+                              bookAuthors={book.authors[0]}
+                              bookCoverUrl={book.imageLinks.thumbnail}
+                              currentBookshelf={book.shelf}
+                              onChangeBookshelf={this.onChangeBookshelf}
+                              bookCoverWidth={128}
+                              bookCoverHeight={193}
+                            />
+                          </li>
+                        ))}
+                      </ol>
+                    </div>
                   </div>
-                </div>
-                <div className="bookshelf">
-                  <BookshelfTitleComp title="Read" />
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {readBookshelfData.map(book => (
-                        <li key={book.id}>
-                          <BookComp
-                            bookId={book.id}
-                            bookTitle={book.title}
-                            bookAuthors={book.authors[0]}
-                            bookCoverUrl={book.imageLinks.thumbnail}
-                            currentBookshelf={book.shelf}
-                            onChangeBookshelf={this.onChangeBookshelf}
-                            bookCoverWidth={128}
-                            bookCoverHeight={193}
-                          />
-                        </li>
-                      ))}
-                    </ol>
+                  <div className="bookshelf">
+                    <BookshelfTitleComp title="Read" />
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                        {readBookshelfData.map(book => (
+                          <li key={book.id}>
+                            <BookComp
+                              bookId={book.id}
+                              bookTitle={book.title}
+                              bookAuthors={book.authors[0]}
+                              bookCoverUrl={book.imageLinks.thumbnail}
+                              currentBookshelf={book.shelf}
+                              onChangeBookshelf={this.onChangeBookshelf}
+                              bookCoverWidth={128}
+                              bookCoverHeight={193}
+                            />
+                          </li>
+                        ))}
+                      </ol>
+                    </div>
                   </div>
                 </div>
               </div>
